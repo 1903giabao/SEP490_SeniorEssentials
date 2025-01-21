a3b9948eb391e0a298d626ba8944100dfce6e8c2
Tivooo <tienv5995@gmail.com>
2025-01-21 22:42:03 +0700

    feat : login/sign up by email

--- SE.API/Controllers/IdentityController.cs
+++ SE.API/Controllers/IdentityController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.Data;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Mvc;
+using SE.Common.Request;
 using SE.Service.Services;
+using SE.Common.DTO;
 
 namespace SE.API.Controllers
 {
@@ -22,8 +27,29 @@ namespace SE.API.Controllers
         public async Task<IActionResult> SendOTP (string email)
         {
             var result  = await _identityService.SendOtpToUser(email);
-
             return Ok(result);
         }
+
+        [AllowAnonymous]
+        [HttpPost("managed-auths/sign-ups")]
+        public async Task<IActionResult> Signup([FromBody] ElderlySignUpModel req)
+        {
+           var result = await _identityService.SignupForElderly(req);
+                  return Ok(result);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("managed-auths/sign-ins")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestModel req)
+        {
+            var loginResult = await _identityService.Login(req.Email, req.Password, req.DeviceToken);
+            var handler = new JwtSecurityTokenHandler();
+
+            var rs = handler.WriteToken( (JwtSecurityToken)loginResult.Data);
+
+
+            return Ok(rs);
+        }
+
     }
 }
--- SE.Service/Services/IdentityService.cs
+++ SE.Service/Services/IdentityService.cs
@@ -6,6 +6,7 @@ using System.Security.Claims;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Firebase.Auth;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Org.BouncyCastle.Asn1.Pkcs;
@@ -16,6 +17,8 @@ using SE.Common.Setting;
 using SE.Data.Models;
 using SE.Data.UnitOfWork;
 using SE.Service.Base;
+using SE.Service.Helper;
+using static System.Net.WebRequestMethods;
 
 namespace SE.Service.Services
 {
@@ -24,6 +27,8 @@ namespace SE.Service.Services
     {
         Task<bool> SendOtpToUser(string email);
         Task<IBusinessResult> SubmitOTP(CreateUserReq req);
+        Task<IBusinessResult> SignupForElderly(ElderlySignUpModel req);
+        Task<IBusinessResult> Login(string email, string password, string deviceToken);
 
     }
 
@@ -126,5 +131,146 @@ namespace SE.Service.Services
                 throw new Exception(ex.Message, ex);
             }
         }
+
+        public async Task<IBusinessResult> SignupForElderly(ElderlySignUpModel req)
+        {
+            try
+            {
+                if (!FunctionCommon.IsValidEmail(req.Email))
+                {
+                    return new BusinessResult(Const.FAIL_CREATE, Const.FAIL_CREATE_MSG, "Wrong email format!");
+                }
+                var user = _unitOfWork.AccountRepository.FindByCondition(u => u.Email == req.Email).FirstOrDefault();
+
+                if (user != null)
+                {
+                    if (user.IsVerified == true)
+                    {
+                        return new BusinessResult(Const.FAIL_CREATE, Const.FAIL_CREATE_MSG, "Existed email");
+
+                    }
+                    else
+                    {
+                        return new BusinessResult(Const.FAIL_CREATE, Const.FAIL_CREATE_MSG, "Email is registered but not verified!");
+                    }
+                }
+
+                var newUser = new Account
+                {
+                    Email = req.Email,
+                    Password = SecurityUtil.Hash(req.Password),
+                    FullName = req.FullName,
+                    Status = "Active",
+                    IsVerified = false,
+                    Avatar = "https://static-00.iconduck.com/assets.00/avatar-icon-2048x2048-aiocer4i.png",
+                    RoleId = 2
+                };
+
+                var res = await _unitOfWork.AccountRepository.CreateAsync(newUser);
+
+       /*         var mailUpdateData = new EmailData
+                {
+                    EmailToId = req.Email,
+                    EmailToName = "Senior Essentials",
+                    EmailBody = "Dang ki thanh cong",
+                    EmailSubject = "XAC NHAN DANG KI THANH CONG"
+                };
+
+                var rsUpdate = await _emailService.SendEmailAsync(mailUpdateData);*/
+
+                if (res < 0)
+                {
+                    return new BusinessResult(Const.FAIL_CREATE, Const.FAIL_CREATE_MSG, "Fail to create new account!");
+                }
+
+                return new BusinessResult(Const.SUCCESS_CREATE, Const.SUCCESS_CREATE_MSG, "Create new account successfully!");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+            }
+        }
+
+        public async Task<IBusinessResult> Login(string email, string password, string deviceToken)
+        {
+            try
+            {
+                if (!FunctionCommon.IsValidEmail(email))
+                {
+                    return new BusinessResult(Const.FAIL_READ, Const.FAIL_READ_MSG, "Wrong email format!");
+                }
+                var user = _unitOfWork.AccountRepository
+                                            .FindByCondition(u => u.Email == email)
+                                            .FirstOrDefault();
+                var hash = SecurityUtil.Hash(password);
+
+                if (user == null || !SecurityUtil.Hash(password).Equals(user.Password))
+                {
+                    return new BusinessResult(Const.FAIL_READ, Const.FAIL_READ_MSG, "Wrong email or password!");
+                }
+
+                if (user.IsVerified == false)
+                {
+                    return new BusinessResult(Const.FAIL_READ, Const.FAIL_READ_MSG, "Email is registered but not verified!");
+
+                }
+
+                var userRole = _unitOfWork.RoleRepository
+                                                .FindByCondition(ur => ur.RoleId == user.RoleId)
+                                                .FirstOrDefault();
+                user.Role = userRole!;
+                if (deviceToken != null)
+                {
+                    user.DeviceToken = deviceToken;
+                }
+                _unitOfWork.AccountRepository.Update(user);
+                return new BusinessResult(Const.SUCCESS_LOGIN, Const.SUCCESS_LOGIN_MSG,CreateJwtToken(user));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+            }
+        }
+
+        public SecurityToken CreateJwtToken(Account user)
+        {
+            try
+            {
+                var utcNow = DateTime.UtcNow;
+                var userRole = _unitOfWork.RoleRepository.FindByCondition(u => u.RoleId == user.RoleId).FirstOrDefault();
+                var authClaims = new List<Claim>
+          {
+              new(JwtRegisteredClaimNames.NameId, user.AccountId.ToString()),
+              new(JwtRegisteredClaimNames.Email, user.Email),
+              new(ClaimTypes.Role, userRole.RoleName),
+              new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+          };
+
+                var key = Encoding.ASCII.GetBytes(_jwtSettings.Key);
+
+                var tokenDescriptor = new SecurityTokenDescriptor()
+                {
+                    Subject = new ClaimsIdentity(authClaims),
+                    SigningCredentials =
+                        new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256),
+                    Expires = utcNow.Add(TimeSpan.FromHours(1)),
+                };
+
+                var handler = new JwtSecurityTokenHandler();
+
+                var token = handler.CreateToken(tokenDescriptor);
+
+                return token;
+            }
+            catch (Exception ex)
+            {
+                return null;
+
+            }
+        }
+
+
+
+
     }
 }
