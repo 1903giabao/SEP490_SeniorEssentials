b986c66218714a3362aec77aeea3b010928b479c
1903giabao <baogavip00@gmail.com>
2025-03-13 01:03:55 +0700

    fix: emergency information

---
+++ SE.Common/Request/Emergency/CreateEmergencyInformationRequest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE.Common.Request.Emergency
+{
+    public class CreateEmergencyInformationRequest
+    {
+        public int ElderlyId { get; set; }
+        public IFormFile FrontCameraImage { get; set; }
+        public IFormFile RearCameraImage { get; set; }
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+    }
+}
