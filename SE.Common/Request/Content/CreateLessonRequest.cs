65e3de1041c4a9c9ec3c31cf0b32f6b3a5101a0f
1903giabao <baogavip00@gmail.com>
2025-03-18 20:41:48 +0700

    fix: emergency and content

--- SE.Common/Request/Content/CreateLessonRequest.cs
+++ SE.Common/Request/Content/CreateLessonRequest.cs
@@ -10,6 +10,7 @@ namespace SE.Common.Request.Content
     public class CreateLessonRequest
     {
         public int AccountId { get; set; }
+        public int PlaylistId { get; set; }
         public string LessonName { get; set; }
         public IFormFile LessonFile { get; set; }
     }
