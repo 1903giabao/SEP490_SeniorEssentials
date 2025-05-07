9e9f3b9b82627719137927a1c5345a12d614415e
Tivooo <tienv5995@gmail.com>
2025-05-07 19:57:41 +0700

    fix : add fields in get professor feedback

--- SE.Common/Response/Professor/GetProfessorRatingVM.cs
+++ SE.Common/Response/Professor/GetProfessorRatingVM.cs
@@ -11,6 +11,19 @@ namespace SE.Common.Response.Professor
         public string CreatedBy { get; set; }
         public string Content { get; set; }
         public int Star {  get; set; }
+        public string Avatar
+        {
+            get; set;
+        }
 
+        public string CreatedByAvatar { get; set; }
+
+        public string FullName { get; set; }
+
+        public string TimeOfAppointment { get; set; }
+
+        public string DateOfAppointment { get; set; }
+
+        public string ReasonOfMeeting { get; set; }
     }
 }
